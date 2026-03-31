b8c629de7ddc20cd57ae060f17b28c9f18d6b052
kfsofronas <kfsofronas@gmail.com>
2026-03-31 00:37:14 -0400

    fix bad casting

--- source/mplayer/sub/spudec.c
+++ source/mplayer/sub/spudec.c
@@ -876,9 +876,9 @@ static void sws_spu_image(unsigned char *d1, unsigned char *d2, int dw, int dh,
 	}
 
 	ctx=sws_getContext(sw, sh, PIX_FMT_GRAY8, dw, dh, PIX_FMT_GRAY8, SWS_GAUSS, &filter, NULL, NULL);
-	sws_scale(ctx,&s1,&ss,0,sh,&d1,&ds);
+	sws_scale(ctx, (const uint8_t * const *)&s1, &ss, 0, sh, (uint8_t * const *)&d1, &ds);
 	for (i=ss*sh-1; i>=0; i--) s2[i] = -s2[i];
-	sws_scale(ctx,&s2,&ss,0,sh,&d2,&ds);
+	sws_scale(ctx, (const uint8_t * const *)&s2, &ss, 0, sh, (uint8_t * const *)&d2, &ds);
 	for (i=ds*dh-1; i>=0; i--) d2[i] = -d2[i];
 	sws_freeContext(ctx);
 }
